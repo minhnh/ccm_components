48375fad87de97c1d9375ccda4ca9d85715059e0
Minh Nguyen <minhnh.91@gmail.com>
2019-05-22 11:35:19 +0200

    check if there's any answers for the question

--- answer_ranking/ccm.answer_ranking.js
+++ answer_ranking/ccm.answer_ranking.js
@@ -190,7 +190,8 @@
             if ( numAnswers === self.constants.num_answer ) break;
 
             // skip the user's own answers
-            if ( ansKey === userData[ 'answers' ][ questionId ][ 'hash' ] ) continue;
+            if ( userData[ 'answers' ][ questionId ] && ansKey === userData[ 'answers' ][ questionId ][ 'hash' ] )
+              continue;
 
             // add to 'answers' if this answers is already ranked by the current user
             if ( username in allAnswers[ ansKey ][ 'ranked_by' ] ) {
