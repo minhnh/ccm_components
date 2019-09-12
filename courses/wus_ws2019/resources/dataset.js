13987141ae2f626d732713f7dfe3e4a258574337
Minh Nguyen <minhnh.91@gmail.com>
2019-09-12 08:50:59 +0200

    add wus_ws2019 course with first lecture

---
+++ courses/wus_ws2019/resources/dataset.js
@@ -0,0 +1,32 @@
+/**
+ * @overview data to configure the components of a course
+ * @author Minh Nguyen <minh.nguyen@smail.inf.h-brs.de>
+ * @copyright 2019
+ * @license The MIT License (MIT)
+ */
+ccm.files[ 'dataset.js' ] = {
+  'course_name': 'WuS WS2019',
+
+  // unique ID for course, to be prepended to 'collection_id' below when accessing database
+  "course_id": "wus_ws2019",
+
+  "store_url": "https://digiklausur.ddns.net",
+
+  "home_menu": {
+    "key": "home_menu",
+    "sections": [
+      {
+        'title': 'Assignment 1', 'id': 'assignment-1',
+
+        "entries": [
+          {
+            "title": "Questions and Answers",
+            "component_name": "question_answer",
+            "collection_id": "wus_ws2019_assignment_1",
+            "entry_type": "qa"
+          }
+        ]
+      }
+    ]
+  }
+};
